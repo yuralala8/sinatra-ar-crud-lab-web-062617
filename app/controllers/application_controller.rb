9f9c79237079c7b5db9c1373cab6556ac7177a69
yuralala8 <yc1435@nyu.edu>
2017-07-21 09:01:06 -0400

    sinatra crud lab

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,46 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do 
-    
+  get '/posts' do
+    @posts = Post.all
+    erb :index
   end
+
+  get '/posts/new' do
+    erb :new
+  end
+
+  get '/posts/:id' do
+    @post = Post.find(params[:id])
+    erb :show
+  end
+
+  post '/posts' do
+    @post = Post.create(params)
+    redirect '/posts'
+  end
+
+  get '/posts/:id/edit' do
+    @post = Post.find(params[:id])
+  erb :edit
+  end
+
+  patch '/posts/:id' do
+    @post = Post.find(params[:id])
+    @post.name = params[:name]
+    @post.content = params[:content]
+    @post.save
+    redirect "/posts/#{@post.id}"
+  end
+
+  get '/delete' do
+  end
+  
+  delete '/posts/:id/delete' do
+      @post = Post.find_by_id(params[:id])
+      @post.delete
+      erb :delete
+  end
+
+
 end
--- app/models/model.rb
+++ app/models/model.rb
@@ -1,1 +1,3 @@
-#Placeholder for a model
+class Post < ActiveRecord::Base
+  
+end
